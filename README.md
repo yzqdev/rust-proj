ce5530987e2980eed9b937534d8eba0ef200f1eb
yzqdev <yzqdev@outlook.com>
2022-05-03 15:37:03 +0800

    add workspace

---
+++ README.md
@@ -0,0 +1,46 @@
+# rust教程
+
+## cargo workspace
+直接在顶部的Cargo.toml添加
+```toml
+[workspace]
+
+members = [
+    "adder",
+]
+
+
+```
+
+然后
+
+### 创建二进制
+
+```shell
+cargo new adder
+```
+运行`cargo build`构建工作空间
+```text
+├── Cargo.lock
+├── Cargo.toml
+├── adder
+│   ├── Cargo.toml
+│   └── src
+│       └── main.rs
+└── target
+
+```
+### 创建lib
+```shell
+cargo new add_one --lib
+```
+
+为了在顶层 add 目录运行二进制 crate，可以通过 -p 参数和包名称来运行 cargo run 指定工作空间中我们希望使用的包
+```shell
+ cargo run -p adder
+```
+
+测试
+```shell
+cargo test -p add_one
+```
--- rust-tutor/src/main.rs
+++ rust-tutor/src/main.rs
@@ -1,9 +1,36 @@
+use clap::Parser;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    println!("Hello, world!");
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    println!("{}",pattern);
-    println!("{}",path);
+    let cmd = clap::Command::new("cargo")
+        .bin_name("cargo")
+        .subcommand_required(true)
+        .subcommand(
+            clap::command!("example").arg(
+                clap::arg!(--"manifest-path" <PATH>)
+                    .required(false)
+                    .allow_invalid_utf8(true),
+            ),
+        );
+    let matches = cmd.get_matches();
+    let matches = match matches.subcommand() {
+        Some(("example", matches)) => matches,
+        _ => unreachable!("clap should ensure we don't get here"),
+    };
+    let manifest_path = matches
+        .value_of_os("manifest-path")
+        .map(std::path::PathBuf::from);
+    println!("{:?}", manifest_path);
 }
